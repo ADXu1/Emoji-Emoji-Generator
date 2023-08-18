b9d0f6990790e18fb24b037ef8e3332627fd1c6b
Chuanji Xu <chuanji_xu@163.com>
2023-08-19 03:04:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,36 @@
 from flask import Flask, render_template, request
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run()
-
 import random
 
-# ...
+app = Flask(__name__)
 
 emojis = {
     'happy': '😄',
     'sad': '😢',
     'love': '❤️',
+    'cool': '😎',
+    'laugh': '😂',
+    'cry': '😭',
+    'angry': '😠',
+    'confused': '😕',
+    'thumbs_up': '👍',
+    'thumbs_down': '👎',
     # Add more keywords and emojis here
 }
+
+def generate_emoji_sentence(keywords):
+    selected_emojis = [emojis[keyword] for keyword in keywords if keyword in emojis]
+    if selected_emojis:
+        return ' '.join(selected_emojis)
+    else:
+        return "No matching emojis found for the given keywords."
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     result = ''
     if request.method == 'POST':
         keywords = request.form['keywords'].lower().split()
-        selected_emojis = [emojis[keyword] for keyword in keywords if keyword in emojis]
-        if selected_emojis:
-            result = ' '.join(selected_emojis)
-        else:
-            result = "No matching emojis found for the given keywords."
+        result = generate_emoji_sentence(keywords)
     return render_template('index.html', result=result)
+
+if __name__ == '__main__':
+    app.run()
