1d3f382b2513a6a1c0b14ad46cb0105ba8781a63
Chuanji Xu <chuanji_xu@163.com>
2023-08-19 03:03:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,26 @@ def index():
 
 if __name__ == '__main__':
     app.run()
+
+import random
+
+# ...
+
+emojis = {
+    'happy': '😄',
+    'sad': '😢',
+    'love': '❤️',
+    # Add more keywords and emojis here
+}
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    result = ''
+    if request.method == 'POST':
+        keywords = request.form['keywords'].lower().split()
+        selected_emojis = [emojis[keyword] for keyword in keywords if keyword in emojis]
+        if selected_emojis:
+            result = ' '.join(selected_emojis)
+        else:
+            result = "No matching emojis found for the given keywords."
+    return render_template('index.html', result=result)
